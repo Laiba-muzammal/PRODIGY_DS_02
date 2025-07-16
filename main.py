a4edb5759dbd34292411b2efb17083ec4d22b7bf
Laiba Muzammal <155936083+Laiba-muzammal@users.noreply.github.com>
2025-07-16 23:39:31 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import matplotlib.pyplot as plt
-import pandas as pd
+from visualize import plot_accident_factors
 
-data = pd.read_csv('task05.csv')
-
-data.groupby(['Time_of_Day', 'Weather','Road_Condition'])['Accidents'].sum().unstack().plot(kind='bar',stacked=True)
-plt.figuresize=((10,6))
-plt.title('Accident Contributing Factors')
-plt.xlabel('Factors')
-plt.ylabel('Number of Accidents')
-plt.show()
+if __name__ == "__main__":
+    plot_accident_factors('data/task05.csv')
